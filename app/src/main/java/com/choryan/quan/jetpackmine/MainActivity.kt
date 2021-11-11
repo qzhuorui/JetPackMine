67d3f8b3ced4975a123b4445c79c8ddba3b02a45
ChoRyan Quan <choryanquan@163.com>
2021-11-11 17:58:19 +0800

    add: 1.MotionLayout

---
+++ app/src/main/java/com/choryan/quan/jetpackmine/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.choryan.quan.jetpackmine
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initClick()
+    }
+
+    private fun initClick() {
+        tv_motionLayout.setOnClickListener {
+            startActivity(Intent(this, MotionLayoutActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/choryan/quan/jetpackmine/MotionLayoutActivity.kt
@@ -0,0 +1,22 @@
+package com.choryan.quan.jetpackmine
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by ChoRyan Quan on 2021/11/11 15:46.
+ * E-mail : qzhuorui@gmail.com
+ * Function :
+ */
+class MotionLayoutActivity : AppCompatActivity(R.layout.activity_motion_layout) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    companion object {
+        private const val TAG = "MotionLayoutActivity"
+    }
+
+}
