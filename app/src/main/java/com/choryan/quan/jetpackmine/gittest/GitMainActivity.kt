17848c05610dadc03a0b79320dab2affb1da8fcf
ChoRyan Quan <choryanquan@163.com>
2021-12-27 10:19:49 +0800

    test: 1.git test多commit合并

---
+++ app/src/main/java/com/choryan/quan/jetpackmine/gittest/GitMainActivity.kt
@@ -0,0 +1,28 @@
+package com.choryan.quan.jetpackmine.gittest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.choryan.quan.jetpackmine.R
+
+class GitMainActivity : AppCompatActivity(R.layout.activity_git_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        val test01 = "test01"
+
+        val test02 = "test02"
+
+        val test03 = "test03"
+
+        val test04 = "test04"
+
+        val test05 = "test05"
+
+        /**
+         * squash：压软(或挤软、压坏、压扁等);把…压(或挤)变形;(使)挤进;塞入;
+         * Interactively rebase from here
+         */
+
+    }
+}
